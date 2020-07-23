e55e1ba51ffc5bc977d89b03c1f01f2e099b2b5e
Christos Vasilakis <christos.vasilakis@arm.com>
2020-07-23 10:05:43 +0100

    update foreground.sh

--- pelion-device-simulator/foreground.sh
+++ pelion-device-simulator/foreground.sh
@@ -1,2 +1,2 @@
-# "The IP address for this environment is [[HOST_IP]]"
-# "The URL to access Port 80 is [[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].[[KATACODA_DOMAIN]]"
+echo "The IP address for this environment is [[HOST_IP]]"
+echo "The URL to access Port 80 is [[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].[[KATACODA_DOMAIN]]"
