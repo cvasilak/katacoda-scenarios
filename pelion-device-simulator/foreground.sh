d5c83e0f857b817711d507581451c313cb7c5f71
Christos Vasilakis <christos.vasilakis@arm.com>
2020-07-24 10:22:34 +0100

    further updates.

--- pelion-device-simulator/foreground.sh
+++ pelion-device-simulator/foreground.sh
@@ -1,2 +1,2 @@
-echo "The IP address for this environment is [[HOST_IP]]"
-echo "The URL to access simulator is https://[[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].[[KATACODA_DOMAIN]]/view/peliondm"
+# echo "The IP address for this environment is [[HOST_IP]]"
+# echo "The URL to access simulator is https://[[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].[[KATACODA_DOMAIN]]/view/peliondm"
